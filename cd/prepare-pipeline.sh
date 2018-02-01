5705f85c3b8e8846afa0fd90a4e75ef15c247991
dudick123 <brian.dudick@outlook.com>
2018-01-31 20:13:59 -0500

    Clean Prepare Pipeline

--- cd/prepare-pipeline.sh
+++ cd/prepare-pipeline.sh
@@ -2,3 +2,6 @@ echo "Executing Prepare Pipeline"
 echo 'Make sure the Docker Environment is ready for the build steps.'
 echo 'Remove Containers which may cause name or port conflicts.'
 echo 'Remove Images which may cause tag conflicts.'
+if [[ "$(docker images -q $REPOSITORY_NAME/$IMAGE_TAG:latest 2> /dev/null)" == "" ]]; then
+  docker image rmi $REPOSITORY_NAME/$IMAGE_TAG:latest
+fi
