19c7ede9ef37e6facb696264b378d80ede5209ef
dudick123 <brian.dudick@outlook.com>
2018-02-01 16:13:01 -0500

    Update pipeline-post-always.sh

--- cd/pipeline-post-always.sh
+++ cd/pipeline-post-always.sh
@@ -1,1 +1,23 @@
 echo 'Running Post Always'
+
+docker container ls
+
+docker container stop $CONTAINER_NAME-$BUILD_NUMBER
+
+sleep 1
+
+docker container ls
+
+docker image ls
+
+docker image rmi $IMAGE_TAG:$BUILD_NUMBER
+
+sleep 1
+
+docker image ls
+
+docker image rmi bdudick/test-web-app-rc1.2.3:latest
+
+sleep 1
+
+docker image ls
