bce254e8ac56a329fcb24b113b9d524cf95aa961
dudick123 <brian.dudick@outlook.com>
2018-02-01 15:35:19 -0500

    Update teardown-pipeline.sh

--- cd/teardown-pipeline.sh
+++ cd/teardown-pipeline.sh
@@ -1,4 +1,4 @@
 echo "Executing Teardown Pipeline"
 docker container stop $CONTAINER_NAME-$BUILD_NUMBER
 docker image rmi $IMAGE_TAG:$BUILD_NUMBER
-docker build --tag $REPOSITORY_NAME/$IMAGE_TAG:latest
+docker image rmi $REPOSITORY_NAME/$IMAGE_TAG:latest
