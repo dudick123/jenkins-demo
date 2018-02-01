98b6f0d4b15c377885071671cdebbf8c3ed2ba55
dudick123 <brian.dudick@outlook.com>
2018-02-01 15:42:33 -0500

    Update teardown-pipeline.sh

--- cd/teardown-pipeline.sh
+++ cd/teardown-pipeline.sh
@@ -1,9 +1,12 @@
 echo "Executing Teardown Pipeline"
 docker container ls
 docker container stop $CONTAINER_NAME-$BUILD_NUMBER -f
+sleep 1
 docker container ls
 docker image ls
 docker image rmi $IMAGE_TAG:$BUILD_NUMBER
+sleep 1
 docker image ls
 docker image rmi bdudick/test-web-app-rc1.2.3:latest
+sleep 1
 docker image ls
