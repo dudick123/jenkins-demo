61c1d219f3b5bc3343a732b49d583f51548d1517
dudick123 <brian.dudick@outlook.com>
2018-02-02 19:38:50 -0500

    Update containers-run-initial.sh

--- cd/containers-run-initial.sh
+++ cd/containers-run-initial.sh
@@ -1,2 +1,2 @@
 echo "Executing Container Run Initial"
-docker container run -d --rm -p 80:80 --name $CONTAINER_NAME-$BUILD_NUMBER $IMAGE_TAG:$BUILD_NUMBER
+docker container run -d --rm -p 8181:80 --name $CONTAINER_NAME-$BUILD_NUMBER $IMAGE_TAG:$BUILD_NUMBER
